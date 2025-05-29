2c039724b3333705f3f280375c7d18419c6269a8
Atiyaramzan <atyaramzan2@gmail.com>
2025-05-29 03:16:40 -0700

    Update mental_health (3).py

--- mental_health (3).py
+++ mental_health (3).py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1zjecxdJ_ZMOJv1vdWHhtzXwRoYAK64ey
 """
 
-!pip install dataprep
+subprocess.check_call([sys.executable, "-m", "pip", "install", "dataprep"])
 
 !pip install --quiet keras-tuner
 
